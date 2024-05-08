02572332f99231b58f9c877b8f5fb1b3fbd1d1c0
Danish Humair <me@danishhumair.com>
2024-05-09 03:06:33 +0500

    updated dataframe

--- results/combine_results.py
+++ results/combine_results.py
@@ -7,12 +7,14 @@ bws_gpt4 = np.load('./bws_gpt4.npy')
 bws_gpt4_color = np.load('./bws_gpt4_color.npy')
 bws_gpt4_color2 = np.load('./bws_gpt4_color2.npy')
 bws_gpt4_only_prompt = np.load('./bws_gpt4_only_prompt.npy')
+bws_clip_only_prompt = np.load('./bws_clip_only_prompt.npy')
 
 sim_clip = np.load('./sim_clip.npy')
 sim_gpt4 = np.load('./sim_gpt4.npy')
 sim_gpt4_color = np.load('./sim_gpt4_color.npy')
 sim_gpt4_color2 = np.load('./sim_gpt4_color2.npy')
 sim_gpt4_only_prompt = np.load('./sim_gpt4_only_prompt.npy')
+sim_clip_only_prompt = np.load('./sim_clip_only_prompt.npy')
 
 # Correct the missing 12th image in bws_gpt4 and sim_gpt4
 bws_gpt4 = np.insert(bws_gpt4, 11, bws_clip[11])
@@ -25,11 +27,13 @@ data = {
     "bws_gpt4_color": bws_gpt4_color,
     "bws_gpt4_color2": bws_gpt4_color2,
     "bws_gpt4_only_prompt": bws_gpt4_only_prompt,
+    "bws_clip_only_prompt": bws_clip_only_prompt,
     "sim_clip": sim_clip,
     "sim_gpt4": sim_gpt4,
     "sim_gpt4_color": sim_gpt4_color,
     "sim_gpt4_color2": sim_gpt4_color2,
-    "sim_gpt4_only_prompt": sim_gpt4_only_prompt
+    "sim_gpt4_only_prompt": sim_gpt4_only_prompt,
+    "sim_clip_only_prompt": sim_clip_only_prompt,
 }
 
 df = pd.DataFrame(data)
